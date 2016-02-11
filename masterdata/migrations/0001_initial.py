aaaa3584c7bba95fa3c9ba61ab7694698fe22919
Vinayak Kaniyarakkal <vinayak.programmer@gmail.com>
2016-02-11 19:35:13 +0530

    Created models and migrated

---
+++ masterdata/migrations/0001_initial.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Country',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('active', models.PositiveIntegerField(default=2, choices=[(0, b'Inactive'), (2, b'Active')])),
+                ('created_on', models.DateTimeField(auto_now_add=True)),
+                ('modified_on', models.DateTimeField(auto_now=True)),
+                ('name', models.CharField(max_length=100)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='State',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('active', models.PositiveIntegerField(default=2, choices=[(0, b'Inactive'), (2, b'Active')])),
+                ('created_on', models.DateTimeField(auto_now_add=True)),
+                ('modified_on', models.DateTimeField(auto_now=True)),
+                ('name', models.CharField(max_length=100)),
+                ('country', models.ForeignKey(to='masterdata.Country')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- masterdata/models.py
+++ masterdata/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseContent(models.Model):
+
+    ACTIVE_CHOICES = ((0, 'Inactive'), (2, 'Active'),)
+    active = models.PositiveIntegerField(choices=ACTIVE_CHOICES,
+                                         default=2)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+
+    #                                        BaseContent
+    class Meta:
+        abstract = True
+
+    #                                        BaseContent
+    def switch(self):
+        self.active = {2: 0, 0: 2}[self.active]
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+
+class Country(BaseContent):
+
+    pass
+
+
+class State(BaseContent):
+
+    country = models.ForeignKey('Country')
