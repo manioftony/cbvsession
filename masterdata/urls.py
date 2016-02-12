1cb0d6c38f26e8107bc997229ccdaf22c385c0d0
Vinayak Kaniyarakkal <vinayak.programmer@gmail.com>
2016-02-12 11:19:04 +0530

    Enabled updating and removed active from the form

--- masterdata/urls.py
+++ masterdata/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns(
     '',
     url(r'^(?P<model>(:?country|state))/list/$', v.List.as_view(), name='list'),
     url(r'^(?P<model>(:?country|state))/create/$', v.Create.as_view(), name='create'),
+    url(r'^(?P<model>(:?country|state))/update/(?P<pk>\d+)/$', v.Update.as_view(), name='update'),
 )
--- masterdata/views.py
+++ masterdata/views.py
@@ -8,16 +8,30 @@ class ManageBase(object):
     def model(self):
         return get_model('masterdata', self.kwargs.get('model'))
 
+    def get_form_class(self):
+
+        self.fields = {
+            'country': ('name', ), 'state': ('country', 'name', )
+        }[self.kwargs.get('model')]
+        return super(ManageBase, self).get_form_class()
+
 
 class List(ManageBase, g.ListView):
-
     template_name = 'masterdata/list.html'
 
 
-class Create(ManageBase, g.CreateView):
+class FormBase(ManageBase):
 
     template_name = 'masterdata/add-edit.html'
 
     def get_success_url(self):
         return "/masterdata/%s/list/" % (
             self.kwargs.get('model'))
+
+
+class Create(FormBase, g.CreateView):
+    pass
+
+
+class Update(FormBase, g.UpdateView):
+    pass
