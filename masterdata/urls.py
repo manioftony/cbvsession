1cd2832a2c0f07abd8759d7c3fd7056e43303da5
Vinayak Kaniyarakkal <vinayak.programmer@gmail.com>
2016-02-12 10:49:32 +0530

    Creating

--- masterdata/urls.py
+++ masterdata/urls.py
@@ -5,4 +5,5 @@ from masterdata import views as v
 urlpatterns = patterns(
     '',
     url(r'^(?P<model>(:?country|state))/list/$', v.List.as_view(), name='list'),
+    url(r'^(?P<model>(:?country|state))/create/$', v.Create.as_view(), name='create'),
 )
--- masterdata/views.py
+++ masterdata/views.py
@@ -1,11 +1,20 @@
-from django.views.generic import ListView
+from django.views import generic as g
 from django.db.models import get_model
 
 
-class List(ListView):
+class List(g.ListView):
 
     @property
     def model(self):
         return get_model('masterdata', self.kwargs.get('model'))
 
     template_name = 'masterdata/list.html'
+
+
+class Create(g.CreateView):
+
+    @property
+    def model(self):
+        return get_model('masterdata', self.kwargs.get('model'))
+
+    template_name = 'masterdata/add-edit.html'
